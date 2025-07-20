9222b3ebe278458ad9adf3312fc4f2f0a14bdb3c
quyen6 <quyenthai.060603@gmail.com>
2025-07-20 14:27:12 +0700

    Update Eslint Rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+// Updated by trungquandev.com's author on May 13 2023
+// Sample Eslint config for React project
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "detect" } },
+  plugins: ["react", "react-hooks", "react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/prop-types": 0,
+    "react/display-name": 0,
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
+    "no-console": 1,
+    "no-lonely-if": 1,
+    "no-unused-vars": 1,
+    "no-trailing-spaces": 1,
+    "no-multi-spaces": 1,
+    "no-multiple-empty-lines": 1,
+    "space-before-blocks": ["error", "always"],
+    "object-curly-spacing": [1, "always"],
+    indent: ["warn", 2],
+    semi: [1, "never"],
+    quotes: ["error", "single"],
+    "array-bracket-spacing": 1,
+    "linebreak-style": 0,
+    "no-unexpected-multiline": "warn",
+    "keyword-spacing": 1,
+    "comma-dangle": 1,
+    "comma-spacing": 1,
+    "arrow-spacing": 1,
   },
-])
+};
