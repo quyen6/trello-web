4dc1a2d425dc1ab5e898c751d6b32b1528fb31b4
quyen6 <quyenthai.060603@gmail.com>
2025-07-21 15:59:33 +0700

    config mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import Button from "@mui/material/Button";
 import AccessAlarmsIcon from "@mui/icons-material/AccessAlarms";
 import HomeIcon from "@mui/icons-material/Home";
 import { useEffect } from "react";
+import Typography from "@mui/material/Typography";
+
 function App() {
   useEffect(() => {}, []);
   return (
     <>
       <div>CONTAINED</div>
+      <Typography variant="body2" color="text.secondary">
+        This is a simple example of using Material-UI components in a React app.
+      </Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 
+import theme from "./theme.js";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
