d28e2ada7967fb2e8453dcca9e5d8105eaf0c3ba
quyen6 <quyenthai.060603@gmail.com>
2025-07-22 22:15:21 +0700

    Relative-Absolute Import

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "~/App.jsx";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import theme from "./theme";
+import theme from "~/theme";
 import InitColorSchemeScript from "@mui/material/InitColorSchemeScript";
 
 createRoot(document.getElementById("root")).render(
