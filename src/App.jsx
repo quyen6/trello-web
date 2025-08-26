76518c1614bf042adfe008124ae5166e80309f2a
quyen6 <quyenthai.060603@gmail.com>
2025-08-26 17:40:25 +0700

    handle Redux Persist, Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import Board from "~/pages/Boards/_id";
 import NotFound from "~/pages/404/NotFound";
 import Auth from "~/pages/Auth/Auth";
 import AccountVerification from "~/pages/Auth/AccountVerification";
+import { useSelector } from "react-redux";
+import { selectorCurrentUser } from "~/redux/user/userSlice";
+
+/**
+ * Giải pháp Clean Code trong việc xác định các route nào cần đăng nhập tài khoản xong thì mới cho truy cập
+ * Sử dụng <Outlet /> của react-router-dom để hiển thị các Child Route (xem cách sử dụng trong App() bên dưới)
+ * https://reactrouter.com/en/main/components/outlet
+ * Một bài hướng dẫn khá đầy đủ:
+ * https://www.robinwieruch.de/react-router-private-routes/
+ */
+const ProtectedRoute = ({ user }) => {
+  if (!user) return <Navigate to="/login" replace={true} />;
+  return <Outlet />;
+};
 
 function App() {
+  const currentUser = useSelector(selectorCurrentUser);
   return (
     <Routes>
       {/* Redirect Route */}
@@ -16,9 +31,13 @@ function App() {
           <Navigate to="/boards/689b5a2f5a34f634e2fe9c8d" replace={true} />
         }
       />
+      {/* ProtectedRoute (Hiểu đơn giản trong dự án của chúng ta là những route chỉ cho truy cập sau khi đã login) */}
+      <Route element={<ProtectedRoute user={currentUser} />}>
+        {/* <Outlet /> của react router dom sẽ chạy vào các child route trong này */}
 
-      {/* React Router Dom /boards /boards/{board_id} */}
-      <Route path="/boards/:boardId" element={<Board />} />
+        {/* React Router Dom /boards /boards/{board_id} */}
+        <Route path="/boards/:boardId" element={<Board />} />
+      </Route>
 
       {/* 404 Not Found */}
       <Route path="*" element={<NotFound />} />
